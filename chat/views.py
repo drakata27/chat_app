fee2e60e58214008f7f72bc1a160aa540917e35f
drakata27 <theall3xander@gmail.com>
2023-09-28 15:58:42 +0100

    Connected front end and back end using websocket

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+
+from .models import Room
+
+@require_POST
+def create_room(request, room_id):
+    name = request.POST.get('name', '')
+    url = request.POST.get('url', '')
+
+    print('name', name)
+    print('url', url)
+
+    Room.objects.create(room_id=room_id, client=name, url=url)
+
+    return JsonResponse({
+        'message': 'room was created'
+    })
--- static/js/main.js
+++ static/js/main.js
@@ -12,7 +12,70 @@ const chatOpen = document.querySelector('#chat_open')
 const chatJoin = document.querySelector('#chat_join') 
 const chatIcon = document.querySelector('#chat_icon') 
 const chatWelcome = document.querySelector('#chat_welcome') 
+
 const chatRoom = document.querySelector('#chat_room') 
+
+const chatUsername = document.querySelector('#chat_name')
+const chatLog = document.querySelector('#chat_log')
+const chatInput = document.querySelector('#chat_message_input')
+const chatSubmit = document.querySelector('#chat_message_submit')
+
+// Functions
+function getCookie(name) {
+    var cookieValue = null
+
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';')
+
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim()
+
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
+
+                break
+            }
+        }
+    }
+
+    return cookieValue
+}
+
+async function JoinRoom() {
+    chatName = chatUsername.value
+
+    const data  = new FormData()
+    data.append('name', chatName)
+    data.append('url', chatWindowURL)
+
+    url = `/api/create-room/${chatRoomId}/`
+
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'X-CSRFToken': getCookie('csrftoken'),
+        }, 
+        body: data
+    })
+    .then((res)=> res.json())
+    .then((data)=> console.log('data', data))
+
+    chatSocket = new WebSocket(`ws://${window.location.host}/ws/${chatRoomId}/`)
+
+    chatSocket.addEventListener('message', function(e) {
+        console.log('Message');
+    })
+
+    chatSocket.addEventListener('open', function(e) {
+        console.log('WebSocket connection opened');
+    })
+
+    chatSocket.addEventListener('close', function(e) {
+        console.log('WebSocket connection closed:', e.code, e.reason);
+    })
+
+
+}
 
 // Event listeners
 chatOpen.addEventListener('click', function(e){
@@ -26,6 +89,8 @@ chatJoin.addEventListener('click', function(e){
 
     chatWelcome.classList.add('hidden')
     chatRoom.classList.remove('hidden')
+
+    JoinRoom()
 })
 
 
