f8f6b9ce1e1087bc9605282e4fdbeb128ff942ac
drakata27 <theall3xander@gmail.com>
2023-09-28 13:14:44 +0100

    Set up models for the messages and rooms

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+
+class Message(models.Model):
+    message_body = models.TextField()
+    sent_by = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        ordering = ('created_at',)
+
+    def __str__(self) -> str:
+        return f'{self.sent_by}'
+    
+class Room(models.Model):
+    WAITING = 'waiting'
+    ACTIVE = 'active'
+    CLOSED = 'closed'
+
+    CHOICES_STATUS = (
+        (WAITING, 'Waiting'),
+        (ACTIVE, 'Active'),
+        (CLOSED, 'Closed'),
+    )
+
+    room_id = models.CharField(max_length=255)
+    client = models.CharField(max_length=255)
+    agent = models.ForeignKey(User, related_name='rooms', blank=True, null=True, on_delete=models.SET_NULL)
+    # A single "room" can have multiple messages, and each message can belong to multiple rooms
+    messages = models.ManyToManyField(Message, blank=True)
+    url = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=7, choices=CHOICES_STATUS, default=WAITING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self) -> str:
+        return f'{self.client - {self.room_id}}'
