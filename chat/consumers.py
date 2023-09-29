444c85e53f3f48bbd4b8be6b06b78b07eacbf0ac
drakata27 <theall3xander@gmail.com>
2023-09-29 13:06:54 +0100

    Set up function for receiving and sending in the consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,6 +2,9 @@ import json
 
 from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.utils.timesince import timesince
+
+from .templatetags.chatextras import initials
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -19,3 +22,41 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         # Leave room
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+    
+    async def receive(self, text_data):
+        # Receive message from websocket (front end)
+        text_data_json = json.loads(text_data)
+        type = text_data_json['type']
+        message = text_data_json['message']
+        name = text_data_json['name']
+        agent = text_data_json.get('agent', '')
+
+        # print
+        print('text data json', text_data_json)
+        print('type', type)
+        print('message', message)
+        print('name', name)
+        print('agent', agent)
+
+        if type == 'message':
+            # Send message to group or room
+            await self.channel_layer.group_send(self.room_group_name, {
+                'type': 'chat_message',
+                'message': message,
+                'name': name,
+                'agent': agent,
+                'initials': initials(name),
+                'created_at': '', #timesince(new_message.created_at),
+            })
+
+    async def chat_message(self, event):
+        print('event', event)
+        # Send message to the websocket (front end)
+        await self.send(text_data=json.dumps({
+            'type': event['type'],
+            'message': event['message'],
+            'name': event['name'],
+            'agent': event['agent'],
+            'initials': event['initials'],
+            'created_at': event['created_at'],
+        }))
