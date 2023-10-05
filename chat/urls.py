31cfaa88c7de83677da7dda5b6cc885c7fcc0cfd
drakata27 <theall3xander@gmail.com>
2023-10-05 18:05:13 +0100

    Worked on user managemement

--- chat/urls.py
+++ chat/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('api/create-room/<str:room_id>/', views.create_room, name='create-room'),
     path('chat-admin/', views.admin, name='admin'),
     path('chat-admin/add-user/', views.add_user, name='add_user'),
+    path('chat-admin/user/<uuid:id>/', views.user_detail, name='user_detail'),
+    path('chat-admin/user/<uuid:id>/edit/', views.edit_user, name='edit_user'),
     path('chat-admin/<str:room_id>/', views.room, name='room'),
+    path('chat-admin/<str:room_id>/delete/', views.delete_room, name='delete_room'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -51,6 +51,54 @@ def room(request, room_id):
     return render(request, 'chat/room.html', context=context)
 
 @login_required
+def delete_room(request, room_id):
+    if request.user.has_perm('room.delete_room'):
+        room = Room.objects.get(room_id=room_id)
+        room.delete()
+
+        messages.success(request,'Room deleted successfully!')
+        return redirect('/chat-admin/')
+    else:
+        messages.error(request, 'You do not have access to delete rooms!')
+        return redirect('/chat-admin/')
+
+@login_required
+def user_detail(request, id):
+    user = User.objects.get(pk=id)
+    rooms = user.rooms.all()
+
+    context = { 
+        'user':user, 
+        'rooms': rooms,
+    }
+
+    return render(request, 'chat/user_detail.html', context=context)
+
+@login_required
+def edit_user(request, id):
+    if request.user.has_perm('user.edit_user'):
+        user = User.objects.get(pk=id)
+
+        if request.method == 'POST':
+            form = EditUserForm(request.POST, instance=user)
+
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'User was edited successfully!')
+                return redirect('/chat-admin/')
+        else:
+            form = EditUserForm(instance=user)
+
+        context = { 
+            'user':user, 
+            'form': form,
+        }
+        return render(request, 'chat/edit_user.html', context=context)
+    else:
+        messages.error(request, 'You do not have permition to edit users!')
+        return redirect('/chat-admin/')
+    
+@login_required
 def add_user(request):
     if request.user.has_perm('user.add_user'):
         if request.method == 'POST':
